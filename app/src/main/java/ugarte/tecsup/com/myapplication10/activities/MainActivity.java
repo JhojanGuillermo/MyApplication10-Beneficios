f51afc23feddca85294269a5ca1aba12a6cad4f2
Jhojan Guillermo <jhojan.guillermo@tecsup.edu.pe>
2017-06-11 08:58:59 -0500

    Accion Logout

--- app/src/main/java/ugarte/tecsup/com/myapplication10/activities/MainActivity.java
+++ app/src/main/java/ugarte/tecsup/com/myapplication10/activities/MainActivity.java
@@ -1,14 +1,23 @@
 package ugarte.tecsup.com.myapplication10.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
 import ugarte.tecsup.com.myapplication10.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private SharedPreferences sharedPreferences;
+    public static final String SHARED_PREFERENCE_NAME = "MyApp";
 
     ImageButton noticias;
     ImageButton eventos;
@@ -46,5 +55,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_logout:
+                callLogout(null);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void callLogout(View view){
+        Log.d(TAG, "Loged");
+            SharedPreferences settings = getSharedPreferences(SHARED_PREFERENCE_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.remove("islogged");
+            editor.commit();
+            finish();
     }
 }
