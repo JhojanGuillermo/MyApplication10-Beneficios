7af5a02d2090aadcb96382b924d665f83bb98a9f
Jhojan Guillermo <jhojan.guillermo@tecsup.edu.pe>
2017-06-13 16:46:16 -0500

    calendario

--- app/src/main/java/ugarte/tecsup/com/myapplication10/fragments/EventsGalleryFragment.java
+++ app/src/main/java/ugarte/tecsup/com/myapplication10/fragments/EventsGalleryFragment.java
@@ -1,14 +1,26 @@
 package ugarte.tecsup.com.myapplication10.fragments;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import ugarte.tecsup.com.myapplication10.R;
 
@@ -18,20 +30,54 @@ import ugarte.tecsup.com.myapplication10.R;
 
 public class EventsGalleryFragment extends Fragment {
 
-    CalendarView calendar;
+//    CalendarView calendar;
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_events_gallery, container, false);
+//
+//        calendar = (CalendarView) view.findViewById(R.id.calendar_events);
+//
+//        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+//            @Override
+//            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+//
+//                Toast.makeText(getActivity().getBaseContext(), "Selected date" +dayOfMonth+"/"+month+"/"+year, Toast.LENGTH_LONG).show();
+//            }
+//        });
+//        return view;
+//    }
+
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_events_gallery, container, false);
 
-        calendar = (CalendarView) view.findViewById(R.id.calendar_events);
+        final CompactCalendarView compactCalendar = (CompactCalendarView) view.findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+        compactCalendar.setFirstDayOfWeek(Calendar.SUNDAY);
 
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        Event ev1 = new Event(Color.GREEN, 1497621600000L, "Teacher's Professional Day");
+        compactCalendar.addEvent(ev1);
+
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+            public void onDayClick(Date dateClicked) {
+                Context context = getActivity().getApplicationContext();
 
-                Toast.makeText(getActivity().getBaseContext(), "Selected date" +dayOfMonth+"/"+month+"/"+year, Toast.LENGTH_LONG).show();
+                if (dateClicked.toString().compareTo("Fri Jun 16 09:00:00 AST 2017") == 0){
+                    Toast.makeText(context, "Teacher's Professional Day", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(context, "No Events Planned for that day", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+
             }
         });
         return view;
